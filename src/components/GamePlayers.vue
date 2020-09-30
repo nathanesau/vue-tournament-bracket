aa81a915122858d0c7ca85ff4e55b9ae7a938281
Nathan Esau <nathanesau1@gmail.com>
2020-09-30 00:28:47 -0400

    popup menu

--- src/components/GamePlayers.vue
+++ src/components/GamePlayers.vue
@@ -1,6 +1,21 @@
 <template>
     <div class="vtb-item-players">
+        <div id="popup">
+            <form id="form">
+                <button type="button" id="close" @mousedown="hidePopupMenu">X</button>
+                <b>Hello world</b>
+                <ul>
+                    <li>Hello world</li>
+                    <img src="https://www.thehansindia.com/images/authorplaceholder.jpg" >
+                </ul>
+            </form>
+        </div>
         <div>
+            <div id="textContainer" class="bordered">
+                <button class="button" @mousedown="showPopupMenu(bracketNode)" >
+                    Info
+                </button>
+            </div>
             <div
                 :class="['vtb-player', 'vtb-player1', getPlayerClass(bracketNode.player1)]"
                 @mouseover="highlightPlayer(bracketNode.player1)"
@@ -33,6 +48,13 @@
             }
         },
         methods: {
+            showPopupMenu(bracketNode) {
+                console.log(bracketNode);
+                document.getElementById("popup").style.display = "block";
+            },
+            hidePopupMenu() {
+                document.getElementById("popup").style.display = "none";
+            },
             getPlayerClass(player) {
                 if (player.winner === null || player.winner === undefined) {
                     return "";
@@ -58,3 +80,57 @@
         }
     };
 </script>
+
+<style scoped>
+.button {
+    display: block;
+    width: 100%;
+}
+body {
+  text-align: center;
+}
+p {
+    background-color: white;
+}
+#popup {
+  display: none;
+  position: absolute;
+  top: 10%;
+  background-color: #ffffff;
+}
+#form {
+  position: absolute;
+  color: black;
+  background-color: white;
+  border: 1px solid #212121;
+  border-radius: 2px;
+  padding: 10px;
+  width: 350px;
+  left: 50%;
+  margin-left: -300px;
+  z-index: 4;
+}
+#close {
+  position: absolute;
+  font-weight: bold;
+  margin-left: 331px;
+  margin-top: -11px;
+  width: 30px;
+  height: 27px;
+}
+input {
+  width: 250px;
+  margin-bottom: 20px;
+}
+.cont {
+  border-radius: 2px;
+  padding: 20px 50px;
+  width: 350px;
+  margin: 10px auto 10px auto;
+  text-align: left;
+  overflow: auto;
+}
+#add {
+  margin: 10px;
+}
+</style>
